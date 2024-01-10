9ef07174d8119252729d83e8cead38a94cc40b5d
Ricardo Mora <rurimoca@gmail.com>
2024-01-10 05:36:32 -0600

    Implementación de los métodos del modelo y depuración de código

--- controller/controller.js
+++ controller/controller.js
@@ -3,15 +3,9 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
-const axios = require('axios');
-const bodyParser = require('body-parser');
-const moment = require('moment');
 const model = require('../model/model.js');
 
 app.use(express.static(path.join(__dirname, '../view/public/')));
-
-// Middleware para analizar application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // Ruta para manejar las solicitudes POST de datos de sensores desde la ESP32
 app.post('/', (req, res) => {
@@ -34,68 +28,39 @@ app.get('/', (req, res) => {
 });
 
 // Rutas GET
-app.get('/getQuality', async (req, res) => {
-    let time = moment().format('YYYY-MM-DD HH:mm:ss'); // Formato de fecha y hora deseado
-    console.log(`[${time}] METHOD: GET ${req.path}`);
+app.get('/getSensorsData', async (req, res) => {
     try {
-        const response = await axios.get('http://192.168.1.147:8080' + req.path);
-        const qualityData = response.data; // Suponiendo que la respuesta contiene los datos de calidad
-        time = moment().format('YYYY-MM-DD HH:mm:ss');
-        console.log(`[${time}] Calidad: ${qualityData} PPM\n`);
+        const airQualityResponse = await model.getQualityData()        
+        const temperatureResponse = await model.getTemperatureData()        
+        const humidityResponse = await model.getHumidityData()
+
+        model.consoleTime(`Particulas en el ambiente: ${airQualityResponse}`);
+        model.consoleTime(`Temperatura: ${temperatureResponse}`);
+        model.consoleTime(`Humedad: ${humidityResponse}`);
+
+        const processedData = await model.processSensorData(
+            airQualityResponse,
+            temperatureResponse,
+            humidityResponse
+        );
         
-        // Enviarlo como respuesta a la solicitud GET
-        res.json({ quality: qualityData });
+        // Enviar los datos procesados como respuesta en formato JSON
+        res.json(processedData);
     } catch (error) {
-        console.error('Error al obtener la calidad:', error);
-        res.status(500).json({ error: 'Ocurrió un error al obtener la calidad' });
-    }
-});
-
-app.get('/getTemperature', async (req, res) => {
-    let time = moment().format('YYYY-MM-DD HH:mm:ss'); // Formato de fecha y hora deseado
-    console.log(`[${time}] METHOD: GET ${req.path}`);
-    try {
-        const response = await axios.get('http://192.168.1.147:8080' + req.path); // Cambiar la ruta según tu API local
-        const temperatureData = response.data; // Suponiendo que la respuesta contiene los datos de temperatura
-        time = moment().format('YYYY-MM-DD HH:mm:ss');
-        console.log(`[${time}] Temperatura: ${temperatureData}°C\n`);
-
-        // Enviarlo como respuesta a la solicitud GET
-        res.json({ temperature: temperatureData });
-    } catch (error) {
-        console.error('Error al obtener la temperatura:', error);
-        res.status(500).json({ error: 'Ocurrió un error al obtener la temperatura' });
-    }
-});
-
-app.get('/getHumidity', async (req, res) => {
-    let time = moment().format('YYYY-MM-DD HH:mm:ss'); // Formato de fecha y hora deseado
-    console.log(`[${time}] METHOD: GET ${req.path}`);
-    try {
-        const response = await axios.get('http://192.168.1.147:8080' + req.path); // Cambiar la ruta según tu API local
-        const humidityData = response.data; // Suponiendo que la respuesta contiene los datos de humedad
-        time = moment().format('YYYY-MM-DD HH:mm:ss');
-        console.log(`[${time}] Humedad: ${humidityData}%\n`);
-
-        // Enviarlo como respuesta a la solicitud GET
-        res.json({ humidity: humidityData });
-    } catch (error) {
-        console.error('Error al obtener la humedad:', error);
-        res.status(500).json({ error: 'Ocurrió un error al obtener la humedad' });
+        model.consoleTime(`Error al obtener los valores:${error}`);
+        res.status(500).json({ error: 'Ocurrió un error al obtener los valores' });
     }
 });
 
 // Al conectar un usuario
 io.on('connection', (socket) => {
-    const time = moment().format('YYYY-MM-DD HH:mm:ss'); // Formato de fecha y hora deseado
-    console.log(`[${time}] Usuario conectado`);
+    model.consoleTime("Usuario conectado");
 
     // Mensaje de registro para verificar eventos
     socket.emit('connected', 'Conexión establecida con el servidor');
 
     socket.on('disconnect', () => {
-        const disconnectTime = moment().format('YYYY-MM-DD HH:mm:ss'); // Hora de desconexión
-        console.log(`[${disconnectTime}] Usuario desconectado`);
+        model.consoleTime("Usuario desconectado");
     });
 });
 
